66f29b9c087ed01d1d55558ea514b865f2c655e3
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-02-13 18:55:37 +0200

    Add some methods to CatalogBff.

--- Catalog/Catalog.Host/Repositories/CatalogItemRepository.cs
+++ Catalog/Catalog.Host/Repositories/CatalogItemRepository.cs
@@ -35,6 +35,57 @@ public class CatalogItemRepository : ICatalogItemRepository
         return new PaginatedItems<CatalogItem>() { TotalCount = totalItems, Data = itemsOnPage };
     }
 
+    public async Task<CatalogItem> GetItemByIdAsync(int id)
+    {
+        var item = await _dbContext.CatalogItems
+            .Include(i => i.CatalogBrand)
+            .Include(i => i.CatalogType)
+            .Where(c => c.Id == id)
+            .FirstOrDefaultAsync();
+
+        return item!;
+    }
+
+    public async Task<IReadOnlyCollection<CatalogItem>> GetItemsByBrandAsync(string brand)
+    {
+        var items = await _dbContext.CatalogItems
+            .Include(i => i.CatalogBrand)
+            .Include(i => i.CatalogType)
+            .OrderBy(c => c.Name)
+            .Where(c => c.CatalogBrand.Brand == brand)
+            .ToListAsync();
+
+        return new List<CatalogItem>(items);
+    }
+
+    public async Task<IReadOnlyCollection<CatalogItem>> GetItemsByTypeAsync(string type)
+    {
+        var items = await _dbContext.CatalogItems
+            .Include(i => i.CatalogBrand)
+            .Include(i => i.CatalogType)
+            .OrderBy(c => c.Name)
+            .Where(c => c.CatalogType.Type == type)
+            .ToListAsync();
+
+        return new List<CatalogItem>(items);
+    }
+
+    public async Task<IReadOnlyCollection<CatalogBrand>> GetBrandsAsync()
+    {
+        var items = await _dbContext.CatalogBrands
+            .ToListAsync();
+
+        return new List<CatalogBrand>(items);
+    }
+
+    public async Task<IReadOnlyCollection<CatalogType>> GetTypesAsync()
+    {
+        var items = await _dbContext.CatalogTypes
+            .ToListAsync();
+
+        return new List<CatalogType>(items);
+    }
+
     public async Task<int?> Add(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName)
     {
         var item = await _dbContext.AddAsync(new CatalogItem
--- Catalog/Catalog.Host/Repositories/Interfaces/ICatalogItemRepository.cs
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogItemRepository.cs
@@ -6,5 +6,10 @@ namespace Catalog.Host.Repositories.Interfaces;
 public interface ICatalogItemRepository
 {
     Task<PaginatedItems<CatalogItem>> GetByPageAsync(int pageIndex, int pageSize);
+    Task<CatalogItem> GetItemByIdAsync(int id);
+    Task<IReadOnlyCollection<CatalogItem>> GetItemsByBrandAsync(string brand);
+    Task<IReadOnlyCollection<CatalogItem>> GetItemsByTypeAsync(string type);
+    Task<IReadOnlyCollection<CatalogBrand>> GetBrandsAsync();
+    Task<IReadOnlyCollection<CatalogType>> GetTypesAsync();
     Task<int?> Add(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
 }
