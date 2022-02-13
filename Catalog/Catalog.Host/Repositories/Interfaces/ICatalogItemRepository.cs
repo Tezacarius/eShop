e7da681796aa956978d4cd09fe94541c38a898e1
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-02-13 18:55:37 +0200

    Add some methods to CatalogBff.

--- Catalog/Catalog.Host/Repositories/Interfaces/ICatalogItemRepository.cs
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogItemRepository.cs
@@ -5,6 +5,11 @@ namespace Catalog.Host.Repositories.Interfaces;
 
 public interface ICatalogItemRepository
 {
-    Task<PaginatedItems<CatalogItem>> GetByPageAsync(int pageIndex, int pageSize, int? brandFilter, int? typeFilter);
+    Task<PaginatedItems<CatalogItem>> GetByPageAsync(int pageIndex, int pageSize);
+    Task<CatalogItem> GetItemByIdAsync(int id);
+    Task<IReadOnlyCollection<CatalogItem>> GetItemsByBrandAsync(string brand);
+    Task<IReadOnlyCollection<CatalogItem>> GetItemsByTypeAsync(string type);
+    Task<IReadOnlyCollection<CatalogBrand>> GetBrandsAsync();
+    Task<IReadOnlyCollection<CatalogType>> GetTypesAsync();
     Task<int?> Add(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
 }
