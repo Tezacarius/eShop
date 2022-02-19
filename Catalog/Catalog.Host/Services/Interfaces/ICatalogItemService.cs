dc906819da29abd66ab978e6704a22fec6fc20bf
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-02-19 17:37:54 +0200

    Add services to builder.

--- Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
@@ -2,7 +2,7 @@ namespace Catalog.Host.Services.Interfaces;
 
 public interface ICatalogItemService
 {
-    Task<int?> Add(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
+    Task<int?> AddAsync(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
     Task Remove(int id);
     Task Update(int id, string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
 }
